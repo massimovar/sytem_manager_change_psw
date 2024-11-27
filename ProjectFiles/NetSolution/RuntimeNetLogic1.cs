160307205bdffafe0234e20715310d822c45ff9e
MassimoV <massimo.variolo@rockwellautomation.com>
2024-11-27 15:02:51 +0100

    Hello System Manager

---
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -0,0 +1,25 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.HMIProject;
+using FTOptix.Retentivity;
+using FTOptix.CoreBase;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+#endregion
+
+public class RuntimeNetLogic1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
